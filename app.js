5ec40809863a2c069d14140b54efbab28dd5fe15
Brendan Nee <brendan@blinktag.com>
2021-03-16 12:00:41 -0700

    Dependency updates and version bump

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ nconf.file('./config.json');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
